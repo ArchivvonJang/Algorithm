641bb497900242bafa1a217b80b56a6b2f21399e
ArchivvonJang <76926509+ArchivvonJang@users.noreply.github.com>
2022-03-16 17:10:38 +0900

    동적계획법 정수삼각형

--- algorithm/training/programmers/Dp_Triangle02.java
+++ algorithm/training/programmers/Dp_Triangle02.java
@@ -1,4 +1,4 @@
-package programmers;import java.awt.image.AbstractMultiResolutionImage;
+package programmers;
 
 //동적계획법 - 정수삼각형
 public class Dp_Triangle02 {
@@ -21,13 +21,16 @@ public class Dp_Triangle02 {
         
         //2. 가장 꼭대기의 값을 넣어준다. 
         dp[0][0] = triangle[0][0];
+        System.out.println("맨 꼭대기 값 --> " + dp[0][0]);
         //3. 가장 왼쪽과 오른쪽 값들만 더해준다.
         for (int i = 1; i < triangle.length; i++) {
+        	System.out.println("왼 오른 더하기 i ->" + i);
+        	
             dp[i][0] = dp[i - 1][0] + triangle[i][0]; // 왼쪽 끝 값
             dp[i][i] = dp[i - 1][i - 1] + triangle[i][i]; // 오른쪽 끝 값
-            
-            System.out.println("왼"+dp[i][0]);
-            System.out.println("오른"+ dp[i][i]);
+          
+            System.out.println(i+" 번째 왼 dp[i][0] -> "+dp[i][0] + " / triangle[i][0] ->" + triangle[i][0]);
+            System.out.println(i+" 번째 오른 dp[i][i] -> "+ dp[i][i]+ " / triangle[i][i] ->" + triangle[i][i]);
         }
         
     	/* j|0  1  2  3  4
@@ -42,6 +45,11 @@ public class Dp_Triangle02 {
         //4. 가장 왼쪽과 오른쪽을 제외한 중간 값들을 순차적으로 더한다.
         for (int i = 2; i < triangle.length; i++) {
             for (int j = 1; j < i; j++) {
+            	System.out.println("중간 값 더하기 i : " + i + " / j :" + j);
+            	System.out.println("i : "+i +" 번째 / j  = "+j+" 번째 , dp[i - 1][j - 1] -> " + dp[i - 1][j - 1] );
+            	System.out.println("i : "+i +" 번째 / j  = "+j+" 번째 , dp[i - 1][j] -> " + dp[i - 1][j]);
+            	System.out.println("i : "+i +" 번째 / j  = "+j+" 번째 , triangle[i][j] -> " +triangle[i][j]);
+            	
                 dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) 
                     + triangle[i][j];
                 System.out.print(dp[i][j]+" ");
@@ -58,7 +66,8 @@ public class Dp_Triangle02 {
     	 */
         
         //해당 경로의 최대값들 == 맨 아래 배열의 값들
-        int maxNum = dp[dp.length - 1][0];
+        int maxNum = dp[dp.length - 1][0]; //배열의 길이는 5인데 총 갯수는 4니까?
+        System.out.println("dp.length : " + dp.length);
         //5. 반복문을 돌면서 최대값을 비교해준다. 
         for (int i = 1; i < triangle.length; i++) {
             int num = dp[dp.length - 1][i]; 
